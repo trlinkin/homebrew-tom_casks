b8fd576907d80fd938f1607105215c71b8c89d87
Thomas Linkin <trlinkin@gmail.com>
2015-08-10 20:42:42 -0500

    Adding my first cask
    This cask will install the stable version of wkhtmltopdf. This is the version needed
    to do my job right and let me shit be boxen-ized.

---
+++ Casks/wkhtmltopdf-stable.rb
@@ -0,0 +1,29 @@
+cask :v1 => 'wkhtmltopdf' do
+  version '0.12.2.1'
+
+  sha256 'c2fd9b39182453ba9672f528e7a503928e51bc6a45c3117da06a5193af338d35'
+  # bitbucket.org is the official download host per the vendor homepage
+  url "https://bitbucket.org/wkhtmltopdf/wkhtmltopdf/downloads/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
+  pkg "wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
+
+  name 'wkhtmltopdf'
+  homepage 'http://wkhtmltopdf.org/'
+  license :gpl
+
+  depends_on :macos => '>= :snow_leopard'
+
+  uninstall :pkgutil => 'org.wkhtmltopdf.wkhtmltox',
+            :delete  => [
+                          '/usr/local/include/wkhtmltox',
+                          '/usr/local/lib/libwkhtmltox.dylib',
+                          "/usr/local/lib/libwkhtmltox.#{version.to_i}.dylib",
+                          "/usr/local/lib/libwkhtmltox.#{version.to_f}.dylib",
+                          "/usr/local/lib/libwkhtmltox.#{version.sub(%r{-.*$},'')}.dylib",
+                          '/usr/local/bin/wkhtmltoimage',
+                          '/usr/local/bin/wkhtmltopdf'
+                        ]
+
+  caveats do
+    files_in_usr_local
+  end
+end
